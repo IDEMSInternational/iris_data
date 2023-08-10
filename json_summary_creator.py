7fc8ec76791acc53bc9e18af2714db1775bdcef2
smborio <smborio@idems.international>
2023-08-10 15:00:27 +0100

    updated script and generated json with data only

--- json_summary_creator.py
+++ json_summary_creator.py
@@ -19,23 +19,7 @@ for n in range(400):
     except FileNotFoundError:
         print(f"Data file {data_path} not found.")
 
-    # Process irishtest_vars files
-    var_path = f'irishtest_vars_{n}.txt'
-    try:
-        with open(var_path, 'r') as var_file:
-            var_lines = var_file.readlines()
-            variables = {}
-            for line in var_lines[:13]:
-                parts = line.strip().split('=')
-                if len(parts) == 2:
-                    variable_name = parts[0].strip('[]')  # Remove [] from variable name
-                    variable_value = float(parts[1].rstrip(';'))
-                    variables[variable_name] = variable_value
-            data[-1]["variables"] = variables
-    except FileNotFoundError:
-        print(f"Variables file {var_path} not found.")
-
-with open('all_data.json', 'w') as json_file:
+with open('simplified_data.json', 'w') as json_file:
     json.dump(data, json_file, indent=2)
 
-print("JSON summary created as all_data.json.")
+print("Simplified JSON data created as simplified_data.json.")
